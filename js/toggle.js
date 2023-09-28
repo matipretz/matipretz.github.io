a8a924164ad27ea22750129db64780efa22e440a
Matias Martin Murad Pretz <30426732+matipretz@users.noreply.github.com>
2023-09-28 13:23:21 -0300

    modified:   js/toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,3 +1,20 @@
+document.getElementById("toggler").addEventListener("click", toggleTheme)
+function toggleTheme() {
+    const head = document.querySelector("head");
+    const toggler = document.createElement("link");
+    toggler.href = "css/dark.css";
+    toggler.rel = "stylesheet";
+    toggler.id = "darkMode";
+
+    if (!!document.getElementById("darkMode")) {
+        head.removeChild(document.getElementById("darkMode"));
+        localStorage.setItem("theme", "light");
+    } else {
+        head.append(toggler);
+        localStorage.setItem("theme", "dark");
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const theme = localStorage.getItem("theme");
     if (theme === "dark") {
@@ -21,31 +38,15 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('viewlink3').addEventListener('click', function () {
         viewShow('view3');
     });
-
     viewShow('view1')
 
     document.getElementById('loader').style.display = 'none';
+
 
     const closeButton = document.querySelector('.close-button');
     const modal = document.getElementById('modal');
     closeButton.addEventListener('click', function () {
         modal.style.display = 'none';
     });
+
 });
-
-document.getElementById("toggler").addEventListener("click", toggleTheme)
-function toggleTheme() {
-    const head = document.querySelector("head");
-    const toggler = document.createElement("link");
-    toggler.href = "css/dark.css";
-    toggler.rel = "stylesheet";
-    toggler.id = "darkMode";
-
-    if (!!document.getElementById("darkMode")) {
-        head.removeChild(document.getElementById("darkMode"));
-        localStorage.setItem("theme", "light");
-    } else {
-        head.append(toggler);
-        localStorage.setItem("theme", "dark");
-    }
-}
